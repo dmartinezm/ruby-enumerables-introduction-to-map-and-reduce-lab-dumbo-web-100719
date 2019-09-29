d913cb5f83f305fe9cfdb12655fd4d7ec33a622a
Diego Martinez <dmartinezm@gmail.com>
2019-09-29 17:44:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,14 +1,20 @@
 # My Code here....
 def map_to_negativize(source_array)
   result_array=[]
+  result_array
 
-  source_array.each do |num|
-    result_array<< num * -1
+  source_array.each do |placeholder|
+    result_array<< placeholder * -1
   end
 
   result_array
 end
 def map_to_no_change(source_array)
+  result_array=[]
+
+  source_array.each do |placeholder|
+    result_array<< placeholder
+  result_array
 end
 def map_to_double(source_array)
 end
