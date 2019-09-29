da08e13647260d34e47c76f5d588bb7f53236820
Diego Martinez <dmartinezm@gmail.com>
2019-09-29 17:47:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -19,6 +19,12 @@ def map_to_no_change(source_array)
 end
 
 def map_to_double(source_array)
+  result_array=[]
+
+  source_array.each do |placeholder|
+    result_array<< placeholder*2
+  end
+  result_array
 end
 def map_to_square(source_array)
 end
